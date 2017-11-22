8cb39b89243dc304230d5762e656ca741fd07add
AnnaH <33048158+annahothai@users.noreply.github.com>
2017-11-21 20:53:13 -0800

    TWD4 file upload
    good good

---
+++ TWD4.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+import itertools  
+from bs4 import BeautifulSoup
+import requests
+from datetime import datetime
+import time
+
+def job():
+	page = requests.get("http://www.thinkgeek.com/product/khol/")
+	soup = BeautifulSoup(page.content, 'html.parser')
+	overall = soup.find(id = "product-actions")
+	avail = overall.find('span', {'class':'in_stock'})
+	print(avail.text)
+	print datetime.now().strftime("%Y-%m-%d %H:%M")
+	
+	with open('out.txt', 'a') as f:
+		f.write(avail.text)
+		f.write(datetime.now().strftime("%Y-%m-%d %H:%M"))
+		time.sleep(20)
+
+if __name__ == '__main__':
+	while True:
+		job()
+		time.sleep(20)
+
+
