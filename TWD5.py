b0ef9cdda888845f5474239b73993b77d20aa008
Milan Donhowe <milanaugust@gmail.com>
2017-12-13 22:50:30 -0800

    Added command line arguments + updated code
    I've added some command line arguments to your program using the click library so the user can specify the number of times the program runs and how long the interval is between each program execution via the command line.
    
    So now for example they could run:
    TWD5.py --count 3 --delay 23
    Which would check the cookbook page every twenty seconds for three iterations.
    
    I also fixed a weird error which was probably caused by the webpage updating its format.  The previous program kept giving me an error whenever I ran it because it couldn't find the span element with the class "in_stock" because the element didn't exist due to the cookbook not being in stock.  I've changed that so now it checks the parent element (a paragraph element with the class "availability") and returns the text value of the following element which will either be a span with the class "in_stock" or "out_of_stock".
    
    Also I added a snippet of code at the top of the program which tries to install the click library if the user hasn't installed it already.

--- TWD5.py
+++ TWD5.py
@@ -6,30 +6,54 @@ import requests
 from datetime import datetime
 import time
 
-#The count sets how many times to run the code
-count = 15
+# Following algorithm tries to install the library click if it hasn't already been installed.
+try:
+    import click
+except:
+    import os
+    try:
+        os.system("pip install click")
+        import click
+    except:
+        try:
+            os.system("C:\Python27\Scripts\pip install click")
+            import click
+        except:
+            print("Please install the python library 'click' to run this program.")
+            import sys
+            sys.exit()
 
-def job():
-	
-	page = requests.get("http://www.thinkgeek.com/product/khol/")
-	soup = BeautifulSoup(page.content, 'html.parser')
-	overall = soup.find(id = "product-actions")
-	avail = overall.find('span', {'class':'in_stock'})
-	print(avail.text)
-	print datetime.now().strftime("%Y-%m-%d %H:%M")
-	
-#Printing the output to an out.txt file
-	with open('out.txt', 'a') as f:
-		f.write(avail.text)
-		f.write(datetime.now().strftime("%Y-%m-%d %H:%M"))
-		
-if __name__ == '__main__':
+
+# Adds commandline argument using the click library and keeps the default value at 15 if nothing is specified.
+@click.command()
+@click.option('--count', default=15, help="number of times to run code.")
+@click.option('--delay', default=30, help="seconds between each webpage check.")
+def main(count, delay):
 	while True:
 		if (count == 0):
 			break
 		else:
 			count -= 1
 			job()
-			time.sleep(30)
+			time.sleep(delay)
 #The code will run every 30 seconds, change this however you want
 		
+
+
+
+def job():
+	
+	page = requests.get("http://www.thinkgeek.com/product/khol/")
+	soup = BeautifulSoup(page.content, 'html.parser')
+	overall = soup.find(id = "product-actions")
+	avail = overall.select("p.availability")
+	print(avail[0].text)
+	print datetime.now().strftime("%Y-%m-%d %H:%M")
+	
+#Printing the output to an out.txt file
+	with open('out.txt', 'a') as f:
+		f.write(avail[0].text)
+		f.write(datetime.now().strftime("%Y-%m-%d %H:%M"))
+		
+if __name__ == '__main__':
+    main()
